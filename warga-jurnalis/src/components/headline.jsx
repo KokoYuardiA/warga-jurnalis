9d592ba57d32cbf786465a0006a6224290059877
KokoYuardiA <92006583+KokoYuardiA@users.noreply.github.com>
2023-10-31 08:14:17 +0800

    feat: update componet headlines

--- warga-jurnalis/src/components/headline.jsx
+++ warga-jurnalis/src/components/headline.jsx
@@ -4,6 +4,8 @@ import NewsCard from './reusable/card-news';
 const HeadlineNews = () => {
   const [news, setNews] = useState([]);
   const apiKey = 'pub_31678e4ba0925e8206ac778a4f7f1f02922cb'; // Ganti dengan kunci API Anda
+  const [currentPage, setCurrentPage] = useState(0);
+  const cardsPerPage = window.innerWidth > 768 ? 3 : 1;
 
   useEffect(() => {
     // Panggil API untuk mendapatkan berita Indonesia
@@ -14,19 +16,50 @@ const HeadlineNews = () => {
       });
   }, [apiKey]);
 
+  const startIndex = currentPage * cardsPerPage;
+  const endIndex = startIndex + cardsPerPage;
+
+  const handlePrevClick = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextClick = () => {
+    if (endIndex < news.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
   return (
-    <div className='px-16'>
+    <div className="px-16">
       <h2 className="text-2xl font-bold mb-4">Headline News - Indonesia</h2>
-      <div className="flex overflow-x-auto gap-2">
-        {news.map((item, index) => (
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+        {news.slice(startIndex, endIndex).map((news, index) => (
           <NewsCard
             key={index}
-            title={item.title}
-            published={item.pubDate}
-            image_url={item.image_url}
-            category={item.category}
+            title={news.title}
+            published={news.published}
+            image_url={news.image_url}
+            category={news.category}
           />
         ))}
+      </div>
+      <div className="mt-4 flex justify-between">
+        <button
+          onClick={handlePrevClick}
+          disabled={currentPage === 0}
+          className="px-2 py-1 bg-gray-200 rounded-md"
+        >
+          Previous
+        </button>
+        <button
+          onClick={handleNextClick}
+          disabled={endIndex >= news.length}
+          className="px-2 py-1 bg-gray-200 rounded-md"
+        >
+          Next
+        </button>
       </div>
     </div>
   );
